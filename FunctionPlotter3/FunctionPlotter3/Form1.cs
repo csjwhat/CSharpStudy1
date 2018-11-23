11da73dbc9a66584fa2134476246c10639ff6795
csjwhats <csjwhats@gmail.com>
2018-11-23 22:23:38 +0900

    チェビシェフの多項式まで

---
+++ FunctionPlotter3/FunctionPlotter3/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyPlotterDelegate;
+
+namespace FunctionPlotter3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Paint -= new PaintEventHandler(Form1_Paint);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // イベントハンドラの登録と再描画;;
+            Paint -= new PaintEventHandler(Form1_Paint);
+            Paint += new PaintEventHandler(Form1_Paint);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Plotter myPlot = new Plotter(ClientSize, e.Graphics);
+            myPlot.Axis();
+
+            // チェビシェフの多項式
+            const int MAX_ORDER = 4;
+            Func<double, double>[] Tn = new Func<double, double>[MAX_ORDER+ 1];
+
+            Tn[0] = x => 1.0;
+            Tn[1] = x => x;
+            Tn[2] = x => 2.0 * x * Tn[1](x) - Tn[0](x);
+            Tn[3] = x => 2.0 * x * Tn[2](x) - Tn[1](x);
+            Tn[4] = x => 2.0 * x * Tn[3](x) - Tn[2](x);
+
+            /* for (int n=2; n<=MAX_ORDER; n++) 
+             * {
+             *   Tn[n] = x => 2.0 * x * Tn[n-1] - Tn[n-2];
+             * }
+             */
+
+            // 2次以上の「チェビシェフの多項式」を描画
+            const int ORDER1 = 2;
+            Pen[] pen = new Pen[MAX_ORDER - ORDER1 + 1];
+            for (int n = 0; n < pen.Length; n++) pen[n] = new Pen(Color.Black, 2);
+            pen[1].DashStyle = DashStyle.Dash;
+            pen[2].DashStyle = DashStyle.Dot;
+
+            for (int n = ORDER1; n <= MAX_ORDER; n++)
+                myPlot.Execute(pen[n - ORDER1], Tn[n]);
+        }
+    }
+}
---
+++ FunctionPlotter3/FunctionPlotter3/MyPlotterDelegate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace MyPlotterDelegate
+{
+    class Plotter
+    {
+        const float X_MAX_ = 2.0f;
+        const float Y_MAX_ = 2.0f;
+        const float DIV_ = 4.0f;
+        const float X_PX_ = 50;
+        const float Y_PX_ = 50;
+        const float WO_ = X_PX_ * DIV_;
+        const float HO_ = Y_PX_ * DIV_;
+        const float DX_ = 0.5f / X_PX_;
+
+        readonly Graphics gr_;
+        readonly PointF[] points_ = new PointF[2 * (int)(X_PX_ * DIV_) + 1];
+        readonly RectangleF rect_ = new RectangleF(-X_PX_ * X_MAX_, -Y_PX_ * Y_MAX_, WO_, HO_);
+
+        public Plotter(Size size, Graphics gr)
+        {
+            gr_ = gr;
+            gr_.ResetTransform();
+            gr_.TranslateTransform(size.Width / 2, size.Height / 2 - 30);
+        }
+
+        // 描画領域を示す四角形と座標軸の描画
+        public void Axis()
+        {
+            // 描画領域を示す四角形の描画
+            gr_.FillRectangle(Brushes.White, rect_);
+
+            Font myFont = new Font("Courier New", 12);
+            // 座標軸と目盛値の描画 x軸
+            for (int n=-(int)X_MAX_; n <= (int)X_MAX_; n++)
+                gr_.DrawLine(Pens.Black, X_PX_ * n, HO_ / 2, X_PX_ * n, -HO_ / 2);
+            for (int n=-(int)X_MAX_;n<= (int)X_MAX_; n += 2)
+                gr_.DrawString(String.Format("{0,2}", n), myFont, Brushes.Black, n * X_PX_ - 17, HO_ / 2 + 6);
+            
+            // 座標軸と目盛値の描画 y軸
+            for (int n=-(int)Y_MAX_; n<=(int)Y_MAX_; n++) 
+                gr_.DrawLine(Pens.Black, -WO_/2, Y_PX_ * n, WO_/2, Y_PX_*n);
+            for (int n = -(int)Y_MAX_; n <= (int)Y_MAX_; n++)
+                gr_.DrawString(String.Format("{0,2}", n), myFont, Brushes.Black, -WO_ / 2 - 26, -Y_PX_ * n - 10); 
+        }
+
+        public void Execute(Pen myPen, Func<double, double> fx)
+        {
+            // 描画点の計算
+            for (int n = 0; n < points_.Length; n++)
+            {
+                float x = DX_ * n - X_MAX_;
+                points_[n] = new PointF(X_PX_ * x, -Y_PX_ * (float)fx(x));
+            }
+
+            gr_.SetClip(rect_);
+            gr_.SmoothingMode = SmoothingMode.AntiAlias;
+
+            gr_.DrawLines(myPen, points_);
+
+            gr_.SmoothingMode = SmoothingMode.Default;
+            gr_.ResetClip();
+        }        
+    }
+}
