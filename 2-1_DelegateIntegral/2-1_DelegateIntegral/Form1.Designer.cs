c63f1ceef77f7bf26e7e2ac96b4642f13f8b0b02
csjwhats <csjwhats@gmail.com>
2018-11-24 20:30:01 +0900

    デリゲートのサンプル１

---
+++ 2-1_DelegateIntegral/2-1_DelegateIntegral/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace _2_1_DelegateIntegral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 2-1_DelegateIntegral/2-1_DelegateIntegral/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_1_DelegateIntegral
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double y1 = Integral(fx1, 1, 2, 10);
+            label1.Text = "y1: " + y1.ToString("0.#####");
+
+            double y2 = Integral(fx2, -1, 1, 100);
+            label2.Text = "y2: " + y2.ToString("0.#####");
+
+            // メソッドとの違い
+            double a0 = 1.0;
+            double b0 = 1.5;
+            Func<double, double> fx3 = x => a0 * x + b0;
+
+            double y3 = Integral(fx3, 1, 2, 100);
+            label3.Text = "y3: " + y3.ToString("0.#####");
+
+            a0 = -1.0;
+            b0 = 2.0;
+            double y4 = Integral(fx3, 1, 2, 100);
+            label4.Text = "y4: " + y4.ToString("0.#####");
+        }
+
+        // デリゲードの宣言
+        private delegate double Integrand(double x);
+
+        // デリゲードを引数に持つメソッド
+        // 台形公式による定積分計算
+        private double Integral(Func<double,double> fx, double x1, double x2, int k)
+        {
+            double dx = (x2 - x1) / (double)k; // 増分
+            double sum = (fx(x1) + fx(x2)) / 2.0;
+            for (double x = x1 + dx; x < x2; x += dx) sum = sum + fx(x);
+
+            return sum * dx;
+        }
+
+        // 被積分関数の定義
+        private double fx1(double x)
+        {
+            return 1.0 / x;
+        }
+
+        // 被積分関数の定義２
+        private double fx2(double x)
+        {
+            return 1.0 / (1 + x * x);
+        }
+    }
+}
