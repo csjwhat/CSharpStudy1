6e52b3f46306e8f4aa047d2355701f5d039c8ab7
csjwhats <csjwhats@gmail.com>
2018-11-24 20:29:45 +0900

    継承のサンプル

---
+++ FuctionPlotter2_Inheritance/FuctionPlotter2_Inheritance/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace FuctionPlotter2_Inheritance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Paint -= new PaintEventHandler(Form1_Paint);
+        }
+
+        // このイベントハンドラで関数のグラフを描画する
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            PlotterBase myPlot = new PlotterFx1(ClientSize, e.Graphics);
+            myPlot.Axis();
+
+            Pen myPen = new Pen(Color.Black, 2);
+            myPlot.Execute(myPen);
+
+            myPlot = new PlotterFx2(ClientSize, e.Graphics);
+            myPen.DashStyle = DashStyle.Dash;
+            myPlot.Execute(myPen);
+
+            myPen.DashStyle = DashStyle.DashDot;
+            (new PlotterFx3(ClientSize, e.Graphics)).Execute(myPen);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Paint -= new PaintEventHandler(Form1_Paint);
+            Paint += new PaintEventHandler(Form1_Paint);
+            Invalidate();
+        }
+    }
+
+    public class PlotterFx1 : PlotterBase
+    {
+        public PlotterFx1(Size size, Graphics gr) : base(size, gr) { }
+        public override double fx(double x) { return x * x - 1.0; }
+    }
+
+    public class PlotterFx2 : PlotterBase
+    {
+        public PlotterFx2(Size size, Graphics gr) : base(size, gr) { }
+        public override double fx(double x) { return 0.5 * x * (x * x - 1.0); }
+    }
+
+    public class PlotterFx3 : PlotterBase
+    {
+        public PlotterFx3(Size size, Graphics gr) : base(size, gr) { }
+        public override double fx(double x) { return -0.5 * x * x + 1.0; }
+    }
+}
---
+++ FuctionPlotter2_Inheritance/FuctionPlotter2_Inheritance/MyPlotterInheritence.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace FuctionPlotter2_Inheritance
+{
+    // 抽象クラス
+    public abstract class PlotterBase
+    {
+        const float X_MAX_ = 2.0f;
+        const float Y_MAX_ = 2.0f;
+        const float DIV_ = 4.0f;
+        const float X_PX_ = 50;
+        const float Y_PX_ = 50;
+        const float WO_ = X_PX_ * DIV_;
+        const float HO_ = Y_PX_ * DIV_;
+        const float DX_ = 0.5f / X_PX_;
+
+        readonly Graphics gr_;
+        readonly PointF[] points_ = new PointF[2 * (int)(X_PX_ * DIV_) + 1];
+        readonly RectangleF rect_ = new RectangleF(-X_PX_ * X_MAX_, -Y_PX_ * Y_MAX_, WO_, HO_);
+
+        // コンストラクタ
+        public PlotterBase(Size size, Graphics gr)
+        {
+            gr_ = gr;
+            // 座標系の設定
+            gr_.ResetTransform();
+            gr_.TranslateTransform(size.Width / 2, size.Height / 2 - 30);
+        }
+
+        public void Axis()
+        {
+            // 描画領域を示す四角形の描画
+            gr_.FillRectangle(Brushes.White, rect_);
+
+            Font myFont = new Font("Courier New", 12);
+            // 座標軸と目盛値の描画 X軸
+            for (int n = -(int)X_MAX_; n <= (int)X_MAX_; n++)
+                gr_.DrawLine(Pens.Black, X_PX_ * n, HO_ / 2, X_PX_ * n, -HO_ / 2);
+            for (int n = -(int)X_MAX_; n <= (int)X_MAX_; n += 2)
+                gr_.DrawString(String.Format("{0,2}", n), myFont, Brushes.Black, n * X_PX_ - 17, HO_ / 2 + 6);
+
+            // 座標軸と目盛値の描画 Y軸
+            for (int n = -(int)Y_MAX_; n <= (int)Y_MAX_; n++)
+                gr_.DrawLine(Pens.Black, -WO_ / 2, Y_PX_ * n, WO_ / 2, Y_PX_ * n);
+            for (int n = -(int)Y_MAX_; n <= (int)Y_MAX_; n += 2)
+                gr_.DrawString(String.Format("{0,2}", n), myFont, Brushes.Black, -WO_ / 2 - 26, -Y_PX_ * n - 10);
+        }
+
+        public void Execute(Pen myPen)
+        {
+            for (int n = 0; n < points_.Length; n++)
+            {
+                float x = DX_ * n - X_MAX_;
+                points_[n] = new PointF(X_PX_ * x, -Y_PX_ * (float)fx(x));
+            }
+            gr_.SetClip(rect_);
+            gr_.SmoothingMode = SmoothingMode.AntiAlias;
+
+            gr_.DrawLines(myPen, points_);
+
+            gr_.SmoothingMode = SmoothingMode.Default;
+            gr_.ResetClip();
+        }
+
+        // 抽象メソッド
+        public abstract double fx(double x);
+    }
+}
